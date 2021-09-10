0b2f40ce9cf1fdaf81c1a42e2a6500f5771f378f
ariful4082 <ariful4082@gmail.com>
2021-09-10 07:31:45 +0600

    working available the join us btn

--- power-x-gym-client/src/components/Share/Banner/Banner.jsx
+++ power-x-gym-client/src/components/Share/Banner/Banner.jsx
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import { Col, Container, Row, Button } from 'react-bootstrap';
+import { useHistory } from 'react-router';
 import './Banner.css'
 
 const Banner = ({ home, title }) => {
@@ -15,6 +16,10 @@ const Banner = ({ home, title }) => {
         //         console.log(data);
         //     })
     }
+    const history= useHistory();
+    const handleJoin = ()=>{
+        history.push('/prices')
+    }
     return (
         <Container className="py-5">
             {home &&
@@ -23,7 +28,7 @@ const Banner = ({ home, title }) => {
                         <article className="banner_article text-white">
                             <h1 className="banner-heading">The best fitness Solution in town</h1>
                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque dignissimos consequuntur, iure repellendus error eveniet deleniti temporibus, ipsum sit blanditiis sunt enim praesentium reiciendis. Voluptatem deserunt voluptates illo quibusdam laboriosam, odio nobis error, ut necessitatibus excepturi illum sed est ea! Cupiditate explicabo tempore ratione beatae inventore nam ullam id aliquam.</p>
-                            <Button className="brand_bg" onClick={handleClassesPost}>Join Us</Button>
+                            <Button className="brand_bg" onClick={handleJoin}>Join Us</Button>
                         </article>
                     </Col>
                     <Col md={6}>
